63063fad2ebaba78dfd1e497ff74a9fee45a8021
Saeed Afghah <saeedafghah@gmail.com>
2025-05-08 00:15:52 +0000

    created int_sales_margin model

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,10 @@
+-- With {{ ref('stg_raw__product')}}
+
+-- margin = revenue - purchase_cost
+-- purchase_cost = quantity * purchase_price
+
+SELECT *
+,ROUND(quantity * purchase_price) AS purchase_cost
+,ROUND(revenue - ROUND(quantity * purchase_price)) AS margin
+FROM {{ ref("stg_raw__sales")}}
+LEFT JOIN {{ ref("stg_raw__product")}} USING (products_id)
