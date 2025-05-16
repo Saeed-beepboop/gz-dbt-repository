94ff4fb8786b3a09be42ed098d0554ef3187f6fb
Saeed Afghah <saeedafghah@gmail.com>
2025-05-16 00:22:07 +0000

    added new campaign daily model, and added it to yml file

---
+++ models/intermediate/int_campaigns_day.sql
@@ -0,0 +1,8 @@
+SELECT
+date_date
+, SUM(ads_cost) AS ads_cost
+, SUM(ads_impressions) AS ads_impressions
+, SUM(ads_clicks) AS ads_clicks
+FROM {{ref ('int_campaigns')}}
+GROUP BY date_date
+ORDER BY date_date DESC
