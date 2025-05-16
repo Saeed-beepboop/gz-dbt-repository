733c9427e1ae56cdc893d788ec76e68cead3858d
Saeed Afghah <saeedafghah@gmail.com>
2025-05-16 02:41:44 +0000

    fixed typo

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,7 +1,7 @@
 SELECT
 date_date
 , SUM(ads_cost) AS ads_cost
-, SUM(ads_impression) AS ads_impression
+, SUM(impression) AS ads_impression
 , SUM(ads_clicks) AS ads_clicks
 FROM {{ref ('int_campaigns')}}
 GROUP BY date_date
