012735bd326495da137989288b0c0b4dc6a176d1
Saeed Afghah <saeedafghah@gmail.com>
2025-05-16 00:39:35 +0000

    added new finance_campaigns_day mart model and added it to yml

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,33 @@
+-- date
+-- ads_margin
+-- average_basket
+-- operational_margin
+-- ads_cost
+-- ads_impression
+-- ads_clicks
+-- quantity
+-- revenue
+-- purchase_cost
+-- margin
+-- shipping_fee
+-- log_cost
+-- ship_cost
+
+SELECT
+fd.date_date AS date
+, (operational_margin - ads_cost) AS ads_margin
+, ROUND(average_basket, 2) AS average_basket
+, operational_margin
+, ads_cost
+, ads_impressions
+, ads_clicks
+, quantity
+, revenue
+, purchase_cost
+, margin
+, shipping_fees
+, logcost AS log_cost
+, ship_cost
+FROM {{ ref("finance_days")}} AS fd
+FULL OUTER {{ref ('finance_campaigns_day')}} AS fcd USING (date_date)
+ORDER BY fd.date_date DESC
